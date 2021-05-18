ad4f6dd739ce93ab31b4eee2c90242d5d623ed33
Jonathan Thompson <jthompson3198@gmail.com>
2021-05-18 12:50:50 -0600

    Worked on getting current location and searching for CVS

--- CVS_Parser/CVS_Parser.py
+++ CVS_Parser/CVS_Parser.py
@@ -1,16 +1,57 @@
+from os import name
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key="AUTH_KEY")
+# from GoogleMapsAPIKey import get_my_key
+
+# API_KEY = get_my_key()
+API_KEY = "key"
+
+gmaps = googlemaps.Client(key=API_KEY)
+
+import requests
+from requests import get
+
+# ip = get("https://api.ipify.org").text
+# print("My public IP address is: {}".format(ip))
+# print(type(ip))
+
+url = "https://ipinfo.io/json"
+r = requests.get(url)
+js = r.json()
+city = js["city"]
+region = js["region"]
+country = js["country"]
+loc = js["loc"]
+org = js["org"]
+postal = js["postal"]
+timezone = js["timezone"]
+readme = js["readme"]
+# js.items()
+# print(city)
+
+user_location = gmaps.reverse_geocode(loc)
+
+CVS_results = gmaps.places_nearby(
+    location=loc,
+    radius=160934,
+    open_now=False,
+    name="CVS Pharmacy",
+    type="pharmacy",
+)
+
+# print(CVS_results)
+
+CVS_results = gmaps.places_nearby(page_token=CVS_results)
 
 # Geocoding an address
-geocode_result = gmaps.geocode("1600 Amphitheatre Parkway, Mountain View, CA")
+# geocode_result = gmaps.geocode("1600 Amphitheatre Parkway, Mountain View, CA")
 
 # Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions(
-    "Sydney Town Hall", "Parramatta, NSW", mode="transit", departure_time=now
-)
+# now = datetime.now()
+# directions_result = gmaps.directions(
+#     "Sydney Town Hall", "Parramatta, NSW", mode="transit", departure_time=now
+# )
